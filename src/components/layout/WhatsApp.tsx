6e422d944d42ba9f1c152f462610f56b97c2edff
gui <guisarria@gmail.com>
2024-06-07 16:26:48 -0300

    whatsapp logo fix

--- src/components/layout/WhatsApp.tsx
+++ src/components/layout/WhatsApp.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { MdWifiCalling3 } from 'react-icons/md';
 import { RiWhatsappFill } from 'react-icons/ri';
 import { Page_Informacoesdecontato } from '../../generated';
+import Image from 'next/image';
 
 interface Props {
   data: Page_Informacoesdecontato;
@@ -26,13 +27,10 @@ const WhatsApp = ({ data }: Props) => {
   }, []);
 
   return (
-    <div className="fixed bottom-6 flex flex-col items-center justify-center right-6 z-50 drop-shadow-md">
+    <div className="fixed bottom-6 flex flex-col items-center justify-center right-44 z-50 drop-shadow-md">
       {device && (
         <a href={`tel:${data.coTelefone}`} aria-label="Botão Telefone">
-          <MdWifiCalling3
-            size={55}
-            className="text-black bg-green rounded-full py-1 px-1 block mb-3"
-          />
+          <Image src={'/whatsapp.png'} alt="whatsapp" width={64} height={64} />
         </a>
       )}
 
@@ -40,7 +38,13 @@ const WhatsApp = ({ data }: Props) => {
         href={`https://api.whatsapp.com/send?phone=55${number}`}
         aria-label="Botão WhatsApp"
       >
-        <RiWhatsappFill size={65} className="text-green" />
+        <Image
+          src={'/whatsapp.png'}
+          alt="whatsapp"
+          width={64}
+          height={64}
+          className="bg-white rounded-full"
+        />
       </a>
     </div>
   );
