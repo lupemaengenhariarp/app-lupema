76b35917ebdf2f38ef54cb6bf1ab9e7de17bc41b
Leandro Martin <leandro.dev@outlook.com.br>
2026-05-15 16:47:20 -0300

    alteracao na pagina

--- src/pages/politica-de-qualidade.tsx
+++ src/pages/politica-de-qualidade.tsx
@@ -11,18 +11,28 @@ interface Props {
 
 const PoliticaDeQualidade: NextPage<Props> = ({ data }) => {
   return (
-    <section className="container py-[40px] md:py-[80px] mt-[100px] sm:mt-[74px]">
-      <h1 className="border-green border sm:border-0 px-4 sm:px-0 py-3 sm:py-0 text-2xl sm:text-3xl text-green mb-8 relative">
-        {data.page?.title}
-      </h1>
-      {
-        <p
-          dangerouslySetInnerHTML={{
-            __html: data.page?.content || '',
-          }}
-          className=""
-        />
-      }
+    <section>
+      <div className="flex flex-col lg:flex-row flex-wrap">
+        <div className='w-full sm:w-[640px] md:w-[460px] lg:w-[614px] xl:w-[768px] 2xl:w-[956px] ml-auto mr-auto xl:mr-0 py-[40px] md:py-[80px] mt-[100px] sm:mt-[74px] pl-4 xl:pl-[20px] 2xl:pl-0 pr-4 sm:pr-0 xl:pr-16 justify-end'>
+          <h1 className="border-0 text-2xl sm:text-3xl text-green mb-8 relative">
+            {data.page?.title}
+          </h1>
+          {
+            <div
+              dangerouslySetInnerHTML={{
+                __html: data.page?.content || '',
+              }}
+              className="content-wp"
+            />
+          }
+        </div>
+        <div className="w-full lg:w-[40%] max-h-96 h-auto lg:max-h-[100dvh] sticky top-0">
+          <img
+            src={data.page?.featuredImage?.node.sourceUrl || ''}
+            className="object-cover h-full w-full"
+          />
+        </div>
+      </div>
     </section>
   );
 };
@@ -39,6 +49,11 @@ export const getStaticProps = async () => {
       {
         title: true,
         content: true,
+        featuredImage: {
+          node: {
+            sourceUrl: true,
+          },
+        },
       },
     ],
     pageBy: [
